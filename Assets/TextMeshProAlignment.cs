47ff784217422114428744dfb8223d179ea58b4e
paulatwarp <paul.sinnett@warpdigital.com>
2021-08-13 00:17:35 +0100

    update

---
+++ Assets/TextMeshProAlignment.cs
@@ -0,0 +1,229 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using TMPro;
+
+[System.Serializable]
+public class TextMeshProAlignmentSettings
+{
+	public string guid;
+	public string hierarchy;
+	public TextAlignmentOptions alignment;
+}
+
+public class PreviewScene : System.IDisposable
+{
+	public string path;
+	UnityEngine.SceneManagement.Scene preview;
+	Transform root;
+	GameObject gameObject;
+
+	public PreviewScene(Canvas canvasPrefab)
+	{
+		preview = EditorSceneManager.NewPreviewScene();
+		Debug.Assert(preview != null);
+		Canvas canvas = PrefabUtility.InstantiatePrefab(canvasPrefab, preview) as Canvas;
+		Debug.Assert(canvas != null);
+		root = canvas.transform;
+	}
+
+	public GameObject LoadPrefab(string guid)
+	{
+		path = AssetDatabase.GUIDToAssetPath(guid);
+		if (!path.StartsWith("Packages"))
+		{
+			GameObject prefab = AssetDatabase.LoadAssetAtPath<GameObject>(path);
+			Debug.Assert(prefab != null);
+			gameObject = PrefabUtility.InstantiatePrefab(prefab, root) as GameObject;
+		}
+		return gameObject;
+	}
+
+	public void Dispose()
+	{
+		EditorSceneManager.ClosePreviewScene(preview);
+	}
+}
+
+
+[CreateAssetMenu()]
+public class TextMeshProAlignment : ScriptableObject
+{
+	public Canvas canvas;
+	public List<TextMeshProAlignmentSettings> settings;
+
+	static string GetName(Transform transform)
+	{
+		string name = "";
+		if (transform.parent != null)
+		{
+			name += $"{GetName(transform.parent)}/";
+		}
+		name += transform.name;
+		return name;
+	}
+
+	public string LoadTextMeshProAlignment(string guid)
+	{
+		string result;
+		using (var preview = new PreviewScene(canvas))
+		{
+			List<string> results = new List<string>();
+			GameObject prefab = preview.LoadPrefab(guid);
+			if (prefab != null)
+			{
+				foreach (var text in prefab.GetComponentsInChildren<TextMeshProUGUI>(true))
+				{
+					string hierarchy = GetName(text.transform);
+					var entry = new TextMeshProAlignmentSettings();
+					entry.guid = guid;
+					entry.hierarchy = hierarchy;
+					entry.alignment = text.alignment;
+					PrefabUtility.RecordPrefabInstancePropertyModifications(text);
+					PrefabUtility.ApplyObjectOverride(text, preview.path, InteractionMode.AutomatedAction);
+					settings.Add(entry);
+					results.Add($"TextMesh {preview.path} {hierarchy} alignment is {text.alignment}");
+				}
+				if (results.Count > 0)
+				{
+					result = string.Join("\n", results);
+				}
+				else
+				{
+					result = $"No TextMeshes on {preview.path}";
+				}
+			}
+			else
+			{
+				result = $"{preview.path} skipped";
+			}
+		}
+		return result;
+	}
+
+	public void GetPrefabsDepthFirst(TextMeshProUGUI text, HashSet<string> guids)
+	{
+		var path = AssetDatabase.GetAssetPath(text);
+		Debug.Assert(path != null, $"Can't find asset path for {GetName(text.transform)}");
+		var guid = AssetDatabase.AssetPathToGUID(path);
+		Debug.Assert(guid != null, $"Can't find guid for path {path}");
+		if (!guids.Contains(guid))
+		{
+			foreach (var child in text.GetComponentsInChildren<TextMeshProUGUI>(true))
+			{
+				if (child != text)
+				{
+					var source = PrefabUtility.GetCorrespondingObjectFromSource<TextMeshProUGUI>(child);
+					if (source != null)
+					{
+						GetPrefabsDepthFirst(source, guids);
+					}
+				}
+			}
+			guids.Add(guid);
+		}
+	}
+
+	public HashSet<string> GetTextMeshProGUIDs()
+	{
+		HashSet<string> guids = new HashSet<string>();
+		foreach (var guid in AssetDatabase.FindAssets("t:prefab", null))
+		{
+			var path = AssetDatabase.GUIDToAssetPath(guid);
+			GameObject prefab = AssetDatabase.LoadAssetAtPath<GameObject>(path);
+			foreach (var text in prefab.GetComponentsInChildren<TextMeshProUGUI>(true))
+			{
+				var source = PrefabUtility.GetCorrespondingObjectFromSource<TextMeshProUGUI>(text);
+				if (source != null)
+				{
+					GetPrefabsDepthFirst(source, guids);
+				}
+			}
+			guids.Add(guid);
+		}
+		return guids;
+	}
+
+	public Dictionary<string, TextMeshProAlignmentSettings> GetSettingsLookup()
+	{
+		var lookup = new Dictionary<string, TextMeshProAlignmentSettings>();
+		foreach (var entry in settings)
+		{
+			lookup.Add($"{entry.guid}:{entry.hierarchy}", entry);
+		}
+		return lookup;
+	}
+
+	public HashSet<string> GetGUIDs()
+	{
+		HashSet<string> guids = new HashSet<string>();
+		foreach (var entry in settings)
+		{
+			guids.Add(entry.guid);
+		}
+		return guids;
+	}
+
+	public string ResetSettings(string guid, Dictionary<string, TextMeshProAlignmentSettings> lookup)
+	{
+		string result; 
+		List<string> results = new List<string>();
+		using (var preview = new PreviewScene(canvas))
+		{
+			GameObject prefab = preview.LoadPrefab(guid);
+			if (prefab != null)
+			{
+				foreach (var text in prefab.GetComponentsInChildren<TextMeshProUGUI>(true))
+				{
+					string hierarchy = GetName(text.transform);
+					string id = $"{guid}:{hierarchy}";
+					if (lookup.ContainsKey(id))
+					{
+						TextMeshProAlignmentSettings original = lookup[id];
+						if (text.alignment != original.alignment)
+						{
+							results.Add($"TextMesh {preview.path} {id} updating alignment from {text.alignment} to {original.alignment}");
+							text.alignment = original.alignment;
+						}
+						else
+						{
+							results.Add($"TextMesh {preview.path} {id} already set");
+						}
+					}
+					else
+					{
+						results.Add($"TextMesh {preview.path} {id} not found");
+					}
+				}
+			}
+			if (results.Count > 0)
+			{
+				result = string.Join("\n", results);
+			}
+			else
+			{
+				result = $"No TextMeshes on {preview.path}";
+			}
+		}
+		return result;
+	}
+
+	public string ReserialiseAsset(string guid)
+	{
+		string result;
+		using (var preview = new PreviewScene(canvas))
+		{
+			if (preview.LoadPrefab(guid) != null)
+			{
+				result = $"reserialised {preview.path}";
+			}
+			else
+			{
+				result = $"skipped {preview.path}";
+			}
+		}
+		return result;
+	}
+}
---
+++ Assets/TextMeshProAlignmentEditor.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using Unity.EditorCoroutines.Editor;
+
+[CustomEditor(typeof(TextMeshProAlignment))]
+public class TextMeshProAlignmentEditor : Editor
+{
+	bool working = false;
+	Dictionary<string, TextMeshProAlignmentSettings> lookup;
+	int total;
+	int item;
+
+	public override bool RequiresConstantRepaint()
+	{
+		return working;
+	}
+
+	public override void OnInspectorGUI()
+	{
+		TextMeshProAlignment alignment = (TextMeshProAlignment)target;
+
+		if (GUILayout.Button("Force reserialise all assets"))
+		{
+			if (!working)
+			{
+				EditorCoroutineUtility.StartCoroutineOwnerless(
+					LoopGUIDs(
+						alignment,
+						AssetDatabase.FindAssets("t:prefab", null),
+						(guid) => alignment.ReserialiseAsset(guid)));
+			}
+		}
+
+		if (GUILayout.Button("Load Text Mesh Pro alignment"))
+		{
+			if (!working)
+			{
+				alignment.settings = new List<TextMeshProAlignmentSettings>();
+				EditorCoroutineUtility.StartCoroutineOwnerless(
+					LoopGUIDs(
+						alignment,
+						AssetDatabase.FindAssets("t:prefab", null),
+						(guid) => alignment.LoadTextMeshProAlignment(guid)));
+			}
+		}
+
+		if (GUILayout.Button("Check Text Mesh Pro alignment"))
+		{
+			if (!working)
+			{
+				lookup = alignment.GetSettingsLookup();
+				HashSet<string> guids = alignment.GetTextMeshProGUIDs();
+				guids.IntersectWith(alignment.GetGUIDs());
+				EditorCoroutineUtility.StartCoroutineOwnerless(
+					LoopGUIDs(
+						alignment,
+						new List<string>(guids).ToArray(),
+						(guid) => alignment.ResetSettings(guid, lookup)));
+			}
+		}
+
+		if (working)
+		{
+			Rect rect = GUILayoutUtility.GetRect(18.0f, 18.0f, "TextField");
+			EditorGUI.ProgressBar(
+				rect,
+				(float)item / (float)total,
+				$"{item} / {total}");
+		}
+
+		DrawDefaultInspector();
+	}
+
+	public delegate string ProcessGUID(string guid);
+
+	IEnumerator LoopGUIDs(TextMeshProAlignment alignment, string[] guids, ProcessGUID processGUID)
+	{
+		working = true;
+		total = guids.Length;
+		item = 0;
+		foreach (var guid in guids)
+		{
+			item++;
+			string message = processGUID(guid);
+			Debug.Log(message);
+			yield return null;
+		}
+		EditorUtility.SetDirty(alignment);
+		working = false;
+	}
+}
